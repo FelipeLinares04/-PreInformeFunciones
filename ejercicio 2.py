917429ae7ad2a4facc53dea711c14947410b3ec8
Carlos Felipe Linares Tejedor <000416436@bga.upb>
2019-09-17 16:03:18 -0500

    hola

--- ejercicio 2.py
+++ ejercicio 2.py
@@ -1,10 +1,20 @@
 print("Defina un numero")
+
 def operacion(a,b):
     x=1
     for i in range(b):
         x=x*a
     return x
+
+
 a = int(input("dijite primer numero"))
-b = int(input("dijite segundo numero"))
+b = int(input("dijite exponente"))
 result=operacion(a,b)
 print(result)
+v = int(input("dijite segundo numero"))
+n = int(input("dijite exponencial"))
+result2=operacion(v,n)
+if result>result2:
+    print("el primer resultado es mayor al segundo")
+else:
+    print("el segundo numero es mayor al primero")
